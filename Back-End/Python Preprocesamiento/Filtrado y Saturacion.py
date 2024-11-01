a5c1409d031d15575473fc2974091e0b8c10c4ae
JesusTrinidadAntonio <skibidy.2112@gmail.com>
2024-11-01 12:42:55 -0600

    Filtrado Y seleccion

--- Back-End/Python Preprocesamiento/Filtrado y Saturacion.py
+++ Back-End/Python Preprocesamiento/Filtrado y Saturacion.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 
@@ -17,24 +18,38 @@ if ruta_imagen:
 
     if imagen is not None:
         # Aplicar el filtro de desenfoque gaussiano
-        for i in range(5):
+        for i in range(10):
             imagen_G = cv2.GaussianBlur(imagen, (5, 5), 0)
             imagen_MB = cv2.medianBlur(imagen_G, 5)
             imagen_B = cv2.bilateralFilter(imagen_MB, d=9, sigmaColor=75, sigmaSpace=75)
     
+        # Convertir la imagen de BGR a HSV
+        imagen_hsv = cv2.cvtColor(imagen_B, cv2.COLOR_BGR2HSV)
+
+        # Multiplicar el canal de saturación por un factor para aumentarlo
+        factor_saturacion = 1.5  # Puedes ajustar este valor
+        imagen_hsv[:, :, 1] = np.clip(imagen_hsv[:, :, 1] * factor_saturacion, 0, 255)
+        
+        # Convertir la imagen de vuelta a BGR
+        imagen_saturada = cv2.cvtColor(imagen_hsv, cv2.COLOR_HSV2BGR)
+
         # Crear ventanas con un tamaño personalizado
         cv2.namedWindow('Imagen original', cv2.WINDOW_NORMAL)
         cv2.resizeWindow('Imagen original', 600, 400)  # Cambia las dimensiones a tu gusto
 
-        cv2.namedWindow('Filtro 3(Bilateral)', cv2.WINDOW_NORMAL)
-        cv2.resizeWindow('Filtro 3(Bilateral)', 600, 400)
+        cv2.namedWindow('Imagen Filtrada', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Imagen Filtrada', 600, 400)  # Cambia las dimensiones a tu gusto
+
+        cv2.namedWindow('Imagen Saturada', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('Imagen Saturada', 600, 400)  # Cambia las dimensiones a tu gusto
 
         # Mostrar las imágenes en las ventanas con el tamaño especificado
         cv2.imshow('Imagen original', imagen)
-       # cv2.imshow('Filtro 1 (GaussianBlur)', imagen_G)
-        #cv2.imshow('Filtro 2 (MedianBlur)', imagen_MB)
-        cv2.imshow('Filtro 3(Bilateral)',imagen_B)
+        cv2.imshow('Imagen Filtrada', imagen_B)
+        cv2.imshow('Imagen Saturada', imagen_saturada)
         cv2.imwrite('C:/Users/jesus/Documents/TT2/Imagenes/ImagenFiltrada.jpg', imagen_B)
+        cv2.imwrite('C:/Users/jesus/Documents/TT2/Imagenes/ImagenSaturada.jpg', imagen_saturada)
+
 
 
         # Esperar a que se cierre la ventana
